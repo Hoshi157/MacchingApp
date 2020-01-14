20769f7ad04d9731e67aca31b00ec6824aa8eb9d
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-01-14 18:25:25 +0900

    Initial Commit

---
+++ macchingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  macchingApp
+//
+//  Created by 福山帆士 on 2020/01/14.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
